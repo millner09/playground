42fd4dcf730a1ced11b54ef1eb0c847e9189e419
Adam Millner <adam.millner@gmail.com>
2020-06-13 11:52:26 -0500

    Added /user put and delete.
    Added myHelloWorldMiddleware.

--- javascript/express-test/index.js
+++ javascript/express-test/index.js
@@ -2,8 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const myHelloWorldMiddleware = (req, res, next) => {
+  console.log("Hello World from middleware!");
+  next();
+};
+
+app.use(myHelloWorldMiddleware);
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+app.post("/", function (req, res) {
+  res.send("Got a POST request");
+});
+
+app.put("/user", function (req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Got a DELETE request at /user");
 });
 
 app.listen(port, () =>
