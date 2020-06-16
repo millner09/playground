c6d843250ab3b3d8142ffdd124cd10e574c02aaf
Adam Millner <adam.millner@gmail.com>
2020-06-16 05:03:27 -0500

    More closure.

--- javascript/closure/closure.js
+++ javascript/closure/closure.js
@@ -8,7 +8,7 @@
 
 // console.log(createdFunction(2));
 
-squareFactory = (numberToSquare) => {
+const squareFactory = (numberToSquare) => {
   let value = numberToSquare;
 
   const squareNumber = () => {
@@ -19,17 +19,33 @@ squareFactory = (numberToSquare) => {
   return squareNumber;
 };
 
-const mySquareFactory = squareFactory(4);
+const numTimesCalledReporter = () => {
+  let numTimesCalled = 0;
+  const numTimesCaller = () => {
+    numTimesCalled++;
+    console.log(`I've been called ${numTimesCalled} times!`);
+  };
 
+  return numTimesCaller;
+};
+
+const mySquareFactory = squareFactory(4);
 console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
-console.log(mySquareFactory());
+
+const numCaller = numTimesCalledReporter();
+numCaller();
+
+const functionCallerFactory = (myFunction) => {
+  const functionCaller = (value) => {
+    return myFunction(value);
+  };
+
+  return functionCaller;
+};
+
+myFunctionCaller = functionCallerFactory((value) => {
+  return value * 2;
+});
+
+console.log(myFunctionCaller(2));
+console.log(myFunctionCaller(4));
